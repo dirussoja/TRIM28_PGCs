e977e5208cee418594cf414e784ece43427603b1
dirussoja <dirussoja@gmail.com>
2025-01-16 20:14:31 -0800

    Update merge_BAM_step3.sh

--- ChIP-seq/H3K9me3/alignment/merge_BAM_step3.sh
+++ ChIP-seq/H3K9me3/alignment/merge_BAM_step3.sh
@@ -1,45 +1,49 @@
 #!/bin/bash
-#$ -N H3K9me3_mm39_merge
+#$ -N H3K9me3_ChIP_bw
 #$ -cwd
-#$ -l h_data=5G,h_rt=8:00:00
-#$ -pe shared 6
+#$ -l h_data=5G,h_rt=6:00:00
+#$ -t 1-6:1
+#$ -pe shared 12
+
+fqs=(
+E105_H3K9me3
+E105_Input
+E135_F_H3K9me3
+E135_F_Input
+E135_M_H3K9me3
+E135_M_Input
+)
 
 source ~/.bashrc
 
 module load samtools/1.15
-
-threads=6
+module load python/3.6.8
 
 reads=/u/scratch/j/jadiruss/H3K9me3_update/merge
-new=/u/scratch/j/jadiruss/H3K9me3_update/align
-readsout=/u/home/j/jadiruss/project-clarka/TRIM28/ChIP-seq/H3K9me3/align
+outdir=/u/scratch/j/jadiruss/H3K9me3_update/bw
 
-#E105 ChIP
-samtools merge -@ $threads -o $reads/E105_H3K9me3_ChIP_rmdup.bam $readsout/SRR10560100_STAR_sorted_nmulti1_rmdup_sorted.bam $readsout/SRR10560104_STAR_sorted_nmulti1_rmdup_sorted.bam $new/SRR13296472_STAR_sorted_nmulti1_rmdup_sorted.bam
-samtools sort -@ $threads $reads/E105_H3K9me3_ChIP_rmdup.bam > $reads/E105_H3K9me3_ChIP_rmdup_sorted.bam
-samtools index -@ $threads -b $reads/E105_H3K9me3_ChIP_rmdup_sorted.bam $reads/E105_H3K9me3_ChIP_rmdup_sorted.bam.bai
+threads=12
 
-#E105 ChIP Input
-samtools merge -@ $threads -o $reads/E105_Input_ChIP_rmdup.bam $readsout/SRR10560101_STAR_sorted_nmulti1_rmdup_sorted.bam $readsout/SRR10560105_STAR_sorted_nmulti1_rmdup_sorted.bam $new/SRR13296477_STAR_sorted_nmulti1_rmdup_sorted.bam
-samtools sort -@ $threads $reads/E105_Input_ChIP_rmdup.bam > $reads/E105_Input_ChIP_rmdup_sorted.bam 
-samtools index -@ $threads -b $reads/E105_Input_ChIP_rmdup_sorted.bam $reads/E105_Input_ChIP_rmdup_sorted.bam.bai
+source /u/home/j/jadiruss/env_python3.6.8/bin/activate
 
-#135 Female ChIP
-samtools merge -@ $threads -o $reads/E135_F_H3K9me3_ChIP_rmdup.bam $readsout/SRR10560108_STAR_sorted_nmulti1_rmdup_sorted.bam $readsout/SRR10560114_STAR_sorted_nmulti1_rmdup_sorted.bam $readsout/SRR13296474_STAR_sorted_nmulti1_rmdup_sorted.bam
-samtools sort -@ $threads $reads/E135_F_H3K9me3_ChIP_rmdup.bam > $reads/E135_F_H3K9me3_ChIP_rmdup_sorted.bam
-samtools index -@ $threads -b $reads/E135_F_H3K9me3_ChIP_rmdup_sorted.bam $reads/E135_F_H3K9me3_ChIP_rmdup_sorted.bam.bai
+COUNT=1
+for fq in "${fqs[@]}"; do
+    if [ $COUNT -eq $SGE_TASK_ID ]; then
 
-#135 Female Input
-samtools merge -@ $threads -o $reads/E135_F_Input_ChIP_rmdup.bam $readsout/SRR10560106_STAR_sorted_nmulti1_rmdup_sorted.bam $readsout/SRR10560112_STAR_sorted_nmulti1_rmdup_sorted.bam $readsout/SRR13296479_STAR_sorted_nmulti1_rmdup_sorted.bam
-samtools sort -@ $threads $reads/E135_F_Input_ChIP_rmdup.bam > $reads/E135_F_Input_ChIP_rmdup_sorted.bam
-samtools index -@ $threads -b $reads/E135_F_Input_ChIP_rmdup_sorted.bam $reads/E135_F_Input_ChIP_rmdup_sorted.bam.bai
+bamCoverage -b $reads/$fq"_ChIP_rmdup_sorted.bam" -o $outdir/$fq"_RPKM_bs10.bw" -bs 10 -p $threads \
+--normalizeUsing RPKM
 
-#135 Male ChIP
-samtools merge -@ $threads -o $reads/E135_M_H3K9me3_ChIP_rmdup.bam $readsout/SRR10560111_STAR_sorted_nmulti1_rmdup_sorted.bam $readsout/SRR10560117_STAR_sorted_nmulti1_rmdup_sorted.bam $readsout/SRR13296476_STAR_sorted_nmulti1_rmdup_sorted.bam
-samtools sort -@ $threads $reads/E135_M_H3K9me3_ChIP_rmdup.bam > $reads/E135_M_H3K9me3_ChIP_rmdup_sorted.bam
-samtools index -@ $threads -b $reads/E135_M_H3K9me3_ChIP_rmdup_sorted.bam $reads/E135_M_H3K9me3_ChIP_rmdup_sorted.bam.bai
+bamCoverage -b $reads/$fq"_ChIP_rmdup_sorted.bam" -o $outdir/$fq"_RPGC_bs10.bw" -bs 10 -p $threads \
+--normalizeUsing RPGC --effectiveGenomeSize 2654621783
 
-#135 Male Input
-samtools merge -@ $threads -o $reads/E135_M_Input_ChIP_rmdup.bam $readsout/SRR10560109_STAR_sorted_nmulti1_rmdup_sorted.bam $readsout/SRR10560115_STAR_sorted_nmulti1_rmdup_sorted.bam $readsout/SRR13296481_STAR_sorted_nmulti1_rmdup_sorted.bam
-samtools sort -@ $threads $reads/E135_M_Input_ChIP_rmdup.bam > $reads/E135_M_Input_ChIP_rmdup_sorted.bam
-samtools index -@ $threads -b $reads/E135_M_Input_ChIP_rmdup_sorted.bam $reads/E135_M_Input_ChIP_rmdup_sorted.bam.bai
+bamCoverage -b $reads/$fq"_ChIP_rmdup_sorted.bam" -o $outdir/$fq"_extend_RPKM_bs10.bw" -bs 10 -p $threads \
+--normalizeUsing RPKM -e
+
+bamCoverage -b $reads/$fq"_ChIP_rmdup_sorted.bam" -o $outdir/$fq"_extend_RPGC_bs10.bw" -bs 10 -p $threads \
+--normalizeUsing RPGC --effectiveGenomeSize 2654621783 -e
+
+fi;
+    COUNT=$[COUNT+1];
+done;
+
+#genome effective size is non-n bases from: http://genomewiki.ucsc.edu/index.php/Mm39_35-way_Genome_size_statistics
