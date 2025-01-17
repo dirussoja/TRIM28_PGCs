4c984cd9f8bfdebdcbd7360840ede0012f29ea8a
dirussoja <dirussoja@gmail.com>
2025-01-16 16:55:46 -0800

    Update Genrich_peakcall_step2.sh

--- ATAC-Seq/peak_calling/Genrich_peakcall_step2.sh
+++ ATAC-Seq/peak_calling/Genrich_peakcall_step2.sh
@@ -9,17 +9,17 @@ source ~/.bashrc
 module load samtools/1.15
 module load python/3.6.8
 
-aligngen=/u/scratch/j/jadiruss/ATAC_new/E105/align
-outgen=/u/scratch/j/jadiruss/ATAC_new/E105/peaks
+aligngen=/u/scratch/j/jadiruss/ATAC_Alluvial_WTonly/align/E105_sort
+outgen=/u/scratch/j/jadiruss/ATAC_Alluvial_WTonly/align/E105_sort
 
 threads=6
 
 source /u/home/j/jadiruss/env_python3.6.8/bin/activate
 
-Genrich -t $aligngen/JAD-E105-ATAC-MWT1_genrich_namesort.bam,$aligngen/JAD-E105-ATAC-MWT2_genrich_namesort.bam,$aligngen/JAD-E105-ATAC-MWT3_genrich_namesort.bam,$aligngen/JAD-E105-ATAC-MWT4_genrich_namesort.bam -j -q 0.05 -o $outgen/E105_MWT_genrich_out.narrowPeak -f $outgen/E105_MWT_genrich_pqvalue.bed
+Genrich -t $aligngen/JAD-E105-ATAC-MWT1_genrich_namesort.bam,$aligngen/JAD-E105-ATAC-MWT2_genrich_namesort.bam,$aligngen/JAD-E105-ATAC-MWT3_genrich_namesort.bam,$aligngen/JAD-E105-ATAC-MWT4_genrich_namesort.bam -j -q 0.01 -o $outgen/E105_MWT_genrich_out.narrowPeak -f $outgen/E105_MWT_genrich_pqvalue.bed
 
-Genrich -t $aligngen/JAD-E105-ATAC-MMUT2_genrich_namesort.bam,$aligngen/JAD-E105-ATAC-MMUT3_genrich_namesort.bam,$aligngen/JAD-E105-ATAC-MMUT4_genrich_namesort.bam,$aligngen/JAD-E105-Male-Mut5_genrich_namesort.bam,$aligngen/JAD-E105-Male-Mut7_genrich_namesort.bam -j -q 0.05 -o $outgen/E105_MMUT_genrich_out.narrowPeak -f $outgen/E105_MMUT_genrich_pqvalue.bed
+Genrich -t $aligngen/JAD-E105-ATAC-MMUT2_genrich_namesort.bam,$aligngen/JAD-E105-ATAC-MMUT3_genrich_namesort.bam,$aligngen/JAD-E105-ATAC-MMUT4_genrich_namesort.bam,$aligngen/JAD-E105-Male-Mut5_genrich_namesort.bam,$aligngen/JAD-E105-Male-Mut7_genrich_namesort.bam -j -o $outgen/E105_MMUT_genrich_out.narrowPeak -f -q 0.01 $outgen/E105_MMUT_genrich_pqvalue.bed
 
-Genrich -t $aligngen/JAD-E105-ATAC-FWT1_genrich_namesort.bam,$aligngen/JAD-E105-ATAC-FWT3_genrich_namesort.bam,$aligngen/JAD-E105-ATAC-FWT4_genrich_namesort.bam,$aligngen/JAD-E105-Female-WT5_genrich_namesort.bam -j -q 0.05 -o $outgen/E105_FWT_genrich_out.narrowPeak -f $outgen/E105_FWT_genrich_pqvalue.bed
+Genrich -t $aligngen/JAD-E105-ATAC-FWT1_genrich_namesort.bam,$aligngen/JAD-E105-ATAC-FWT3_genrich_namesort.bam,$aligngen/JAD-E105-ATAC-FWT4_genrich_namesort.bam,$aligngen/JAD-E105-Female-WT5_genrich_namesort.bam -j -q 0.1 -o $outgen/E105_FWT_genrich_out.narrowPeak -f $outgen/E105_FWT_genrich_pqvalue.bed
 
-Genrich -t $aligngen/JAD-E105-F-MUT1_genrich_namesort.bam,$aligngen/JAD-E105-F-MUT4_genrich_namesort.bam,$aligngen/JAD-E105-Female-Mut5_genrich_namesort.bam,$aligngen/JAD-E105-Female-Mut6_genrich_namesort.bam -j -q 0.05 -o $outgen/E105_FMUT_genrich_out.narrowPeak -f $outgen/E105_FMUT_genrich_pqvalue.bed
+Genrich -t $aligngen/JAD-E105-F-MUT1_genrich_namesort.bam,$aligngen/JAD-E105-F-MUT4_genrich_namesort.bam,$aligngen/JAD-E105-Female-Mut5_genrich_namesort.bam,$aligngen/JAD-E105-Female-Mut6_genrich_namesort.bam -j -q 0.01 -o $outgen/E105_FMUT_genrich_out.narrowPeak -f $outgen/E105_FMUT_genrich_pqvalue.bed
