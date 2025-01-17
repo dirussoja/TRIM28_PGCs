eed39a881fc03db757aca78adf066d6c527438d3
dirussoja <dirussoja@gmail.com>
2025-01-16 20:48:02 -0800

    Create subtract_RPKM_bw_step5.sh

---
+++ ChIP-seq/TRIM28/alignment/subtract_RPKM_bw_step5.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#$ -N TRIM28_ChIP_bamCompare
+#$ -cwd
+#$ -l h_data=5G,h_rt=12:00:00
+#$ -t 1-2:1
+#$ -pe shared 12
+
+fqs=(
+E125_F
+E125_M
+)
+
+
+source ~/.bashrc
+
+module load samtools/1.15
+module load python/3.6.8
+
+reads=/u/scratch/j/jadiruss/TRIM28_ChIP/merge
+out=/u/scratch/j/jadiruss/TRIM28_ChIP/bw
+
+threads=12
+
+source /u/home/j/jadiruss/env_python3.6.8/bin/activate
+
+COUNT=1
+for fq in "${fqs[@]}"; do
+    if [ $COUNT -eq $SGE_TASK_ID ]; then
+
+bamCompare -b1 $reads/$fq"_merge_sorted.bam" -b2 $reads/$fq"_Inp_merge_sorted.bam" \
+-o $out/$fq"_RPKM_subtract_bs10.bw" --scaleFactorsMethod None --normalizeUsing RPKM --operation subtract -bs 10 -e --smoothLength 20 -p $threads 
+
+bamCompare -b1 $reads/$fq"_merge_sorted.bam" -b2 $reads/$fq"_Inp_merge_sorted.bam" \
+-o $out/$fq"_RPKM_log2_bs10.bw" --scaleFactorsMethod None --normalizeUsing RPKM --operation log2 --pseudocount 1 -bs 10 -e --smoothLength 20 -p $threads 
+
+
+fi;
+    COUNT=$[COUNT+1];
+done;
